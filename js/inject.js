7634b8f4079e8fe710ae239799a65979de5d1e7e
runoobjerry <realjerry.ishere@gmail.com>
2021-10-09 13:57:30 +0800

    Site updated: 2021-10-09 13:57:30

--- js/inject.js
+++ js/inject.js
@@ -2,12 +2,12 @@ $(window).scroll(function(e) {
 
   var scrollSeviye = $(this).scrollTop();
   
-  if(scrollSeviye>600){
-    scrollSeviye=600;
+  if(scrollSeviye>500){
+    scrollSeviye=500;
   }
   
-  var buyutme=(scrollSeviye/600)+1;
+  var buyutme=(scrollSeviye/100)+1;
  
-  $('#blur').css('backdrop-filter', 'blur('+scrollSeviye/120+'px)');
-  $('#blur').css('-webkit-backdrop-filter', 'blur('+scrollSeviye/120+'px)');
+  $('#blur').css('backdrop-filter', 'blur('+scrollSeviye/100+'px)');
+  $('#blur').css('-webkit-backdrop-filter', 'blur('+scrollSeviye/100+'px)');
 });
